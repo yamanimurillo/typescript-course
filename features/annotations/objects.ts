99f38b917b36b22333beb8048e6066755f6f0aa6
yamanimurillo <yamani.murillo@gmail.com>
2022-12-27 14:19:12 -0700

    annotations on objects

---
+++ features/annotations/objects.ts
@@ -0,0 +1,13 @@
+const profile = {
+    name: 'alex',
+    age: 20,
+    coords: {
+        lat: 0,
+        lng: 15,
+    },
+    setAge(age: number): void {
+        this.age = age;
+    }
+}
+
+const { age }: { age: number } = profile;
