045cd99a58bcb920c03fbae89be1da873b672ec4
yamanimurillo <yamani.murillo@gmail.com>
2022-12-27 13:23:56 -0700

    annotations on functions parameters

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -11,3 +11,21 @@ function divide(a: number, b: number): number {
 function multiply(a: number, b: number): number {
     return a * b;
 }
+
+const logger = (message: string): void => {
+    console.log(message);
+}
+
+const throwError = (error: string): never => {
+    throw new Error(error);
+}
+
+interface Forecast {
+    date: Date,
+    weather: string,
+}
+
+const logForecast = ({date, weather}: Forecast): void => {
+    console.log(date);
+    console.log(weather);
+}
